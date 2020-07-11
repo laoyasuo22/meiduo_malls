54fd770592eb5db66d93787a858b2c437eed33cf
laoysu <wozhiainia007@gmai.com>
2020-07-11 19:36:07 +0800

    注册apps 并导包

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\d\meiduo_mall\venv"
+$env:VIRTUAL_ENV="D:\d\django06\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
